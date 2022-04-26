fef95fee3f71f939a485684823dd8b0b783df93a
witktom5 <witktom5@gmail.com>
2022-04-26 12:27:04 +0200

    change about card bg light

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -1,6 +1,6 @@
 function AboutCard({ title, children }) {
   return (
-    <div className='card bg-base-200 shadow-xl'>
+    <div className='card bg-base-200 bg-about-card shadow-xl'>
       <div className='card-body px-8'>
         <h3 className='card-title mb-8 text-2xl w-fit mx-auto'>{title}</h3>
         {children}
